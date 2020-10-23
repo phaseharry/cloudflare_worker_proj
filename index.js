a566cd327e016139f7f056d642afafa3ba04b025
Harry <harrychenhc@icloud.com>
2020-10-23 12:34:25 -0400

    add htmlrewriter

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const links = [
     name: 'Cloudflare',
     url: 'https://www.cloudflare.com/'
   }
-];
+]
 
 const staticHtmlInfo = {
   url: 'https://static-links-page.signalnerve.workers.dev',
@@ -26,8 +26,37 @@ const staticHtmlInfo = {
   }
 }
 
+class LinksTransformer {
+  constructor(links) {
+    this.links = links
+  }
+
+  async element(element) {
+    if (element.getAttribute('id')) {
+      this.links.forEach(link => {
+        element.append(`\n<a href="${link.url}">${link.name}</a>`)
+      })
+    }
+  }
+
+  comments(comment) {
+    // console.log(comment)
+    // An incoming comment
+  }
+
+  text(text) {
+
+    // An incoming piece of text
+  }
+}
+
+// async function handleRequest(req) {
+//   const res = await fetch(req)
+
+//   return new HTMLRewriter().on("div", new ElementHandler()).transform(res)
+// }
+
 addEventListener('fetch', event => {
-  console.log(event);
   event.respondWith(handleRequest(event.request))
 })
 
@@ -36,9 +65,10 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const { url } = request;
-  if (url.indexOf('/links') >= url.length - 7) {
-    const json = JSON.stringify({ links });
+  const { url } = request
+  const parts = url.split('/');
+  if (parts[parts.length - 1] === 'links') {
+    const json = JSON.stringify({ links })
     return new Response(json, {
       headers: {
         'content-type': 'application/json;charset=UTF-8'
@@ -46,20 +76,17 @@ async function handleRequest(request) {
     })
   }
   try {
-    const { url, reqInfo } = staticHtmlInfo;
-    const res = await fetch(url, reqInfo);
-    const htmlString = await convertResponseToString(res);
-    return new Response(htmlString, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    })
+    const { url, reqInfo } = staticHtmlInfo
+    const res = await fetch(url, reqInfo)
+    // const htmlString = await convertResponseToString(res)
+    return new HTMLRewriter()
+      .on("*", new LinksTransformer(links)).transform(res)
   } catch (err) {
     console.error(err);
   }
 }
 
-/**
+/** probably dont need
  * converts response body to text
  * @param {Request} response
  */
